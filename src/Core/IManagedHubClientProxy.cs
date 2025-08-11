de8bedd200dc9afa0a23d8b31c11cb41615bce61
Faraz <farazzbhn@gmail.com>
2025-08-12 01:10:09 +0330

    works

--- src/Core/IManagedHubClientProxy.cs
+++ src/Core/IManagedHubClientProxy.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ManagedLib.ManagedSignalR.Core;
 public interface IManagedHubClientProxy : IManagedHubClient
@@ -22,7 +17,7 @@ public class ManagedHubClientProxy : IManagedHubClientProxy
         HubType = hubType;
     }
 
-    // Forward the call to the underlying SignalR proxy
+    // Forwards the call to the underlying SignalR proxy
     public Task InvokeClient(string topic, string payload)
     {
         return _client.InvokeClient(topic, payload);
@@ -39,12 +34,17 @@ public class ManagedHubClientProxy : IManagedHubClientProxy
             _hub = hub;
             _hubType = hubType;
         }
-
         public ManagedHubClientProxy All => new(_hub.Clients.All, _hubType);
-        public ManagedHubClientProxy Others => new(_hub.Clients.Others, _hubType);
         public ManagedHubClientProxy Caller => new(_hub.Clients.Caller, _hubType);
-
-        // You can add more as needed, like .Group(), .Client(), etc.
+        public ManagedHubClientProxy Others => new(_hub.Clients.Others, _hubType);
+        public ManagedHubClientProxy OthersInGroup(string groupName) => new(_hub.Clients.OthersInGroup(groupName), _hubType);
+        public ManagedHubClientProxy Client(string connectionId) => new(_hub.Clients.Client(connectionId), _hubType);
+        public ManagedHubClientProxy Clients(IReadOnlyList<string> connectionIds) => new(_hub.Clients.Clients(connectionIds), _hubType);
+        public ManagedHubClientProxy Group(string groupName) => new(_hub.Clients.Group(groupName), _hubType);
+        public ManagedHubClientProxy Groups(IReadOnlyList<string> groupNames) => new(_hub.Clients.Groups(groupNames), _hubType);
+        public ManagedHubClientProxy User(string userId) => new(_hub.Clients.User(userId), _hubType);
+        public ManagedHubClientProxy Users(IReadOnlyList<string> userIds) => new(_hub.Clients.Users(userIds), _hubType);
     }
+}
 
 }
